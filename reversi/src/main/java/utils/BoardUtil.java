0cf14faf2d47be1fadfdacc033b258b93f841878
yupingtao <626683392@qq.com>
2019-05-06 17:47:55 +0800

    删除部分冗余代码

--- reversi/src/main/java/utils/BoardUtil.java
+++ reversi/src/main/java/utils/BoardUtil.java
@@ -28,55 +28,6 @@ import static common.Constant.SIZE;
 public class BoardUtil {
 
 	/**
-	 * 拷贝棋盘二维数组
-	 */
-	public static void copyBinaryArray(byte[][] src, byte[][] dest) {
-		for (int i = 0; i < src.length; i++) {
-			System.arraycopy(src[i], 0, dest[i], 0, src[i].length);
-		}
-	}
-
-	/**
-	 * 拷贝泛型二维数组
-	 *
-	 * @param <T>
-	 * @return
-	 */
-	public static<T extends Cloneable> T[][] copyBinary(IntFunction<T[]> desc1,IntFunction<T[][]> desc2,T[][] src){
-		T[][] desc = desc2.apply(src.length);
-		for (int i = 0; i < src.length; i++) {
-			T[] ts = desc1.apply(src[i].length);
-			for (int i1 = 0; i1 < src[i].length; i1++) {
-				ts[i1] = src[i][i1];
-			}
-		}
-		return desc;
-	}
-
-	/**
-	 * 深度复制方法,需要对象及对象所有的对象属性都实现序列化
-	 * @param src
-	 * @param <T>
-	 * @return
-	 */
-	private static<T extends Serializable> T deepCopy(T src){
-		T outer = null;
-		try {
-			// 将该对象序列化成流,因为写在流里的是对象的一个拷贝，而原对象仍然存在于JVM里面。所以利用这个特性可以实现对象的深拷贝
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			oos.writeObject(src);
-			// 将流序列化成对象
-			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-			ObjectInputStream ois = new ObjectInputStream(bais);
-			outer = (T) ois.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return outer;
-	}
-
-	/**
 	 * 棋子动画
 	 * 		1 --- 6
 	 *     白     黑
@@ -188,7 +139,6 @@ public class BoardUtil {
 	 * @return
 	 */
 	public static BoardData copyBoard(BoardData data) {
-//		return deepCopy(data);
 		return data.cloneData();
 	}
 }
