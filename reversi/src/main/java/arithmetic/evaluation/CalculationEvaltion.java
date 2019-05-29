a321d3cbe44c850439771c4dfc756b1a1fbf3ef6
yupingtao <626683392@qq.com>
2019-05-29 11:56:02 +0800

    增加稳定子计算 黑白棋v1.11

---
+++ reversi/src/main/java/arithmetic/evaluation/CalculationEvaltion.java
@@ -0,0 +1,78 @@
+package arithmetic.evaluation;
+
+import bean.BoardChess;
+import common.Constant;
+import common.WeightEnum;
+
+import static common.Constant.MODEL;
+
+/**
+ * @author ypt
+ * @ClassName CalculationEvaltion 计算估值
+ * @date 2019/5/29 11:43
+ */
+public class CalculationEvaltion implements Evaltion{
+
+    /**
+     * 根据权重类型获取分数
+     * @param weightEnum
+     * @param data
+     * @return
+     */
+    @Override
+    public final int weightScore(WeightEnum weightEnum, int weight, BoardChess data) {
+        byte[] chess = data.getChess();
+        byte player = data.getCurrMove(), other = player == Constant.WHITE ? Constant.BLACK : Constant.WHITE;
+        switch (weightEnum){
+            case MOBILITY:
+                return weight * (data.getOurMobility() - data.getOppMobility());
+            case POSVALUE:
+                return weight * (posEvaluation(chess,player) - posEvaluation(chess,other));
+            case COUNT:
+                return (player == Constant.WHITE ? weight : -weight) * (data.getwCount() - data.getbCount());
+            case STABISTOR:
+                return (player == Constant.WHITE ? weight : -weight) * (data.getwStators().size() - data.getbStators().size());
+            case INNER:
+                return 0;
+            case FRONTIERS:
+                return 0;
+            case PARITY:
+                return 0;
+            default:break;
+        }
+        return 0;
+    }
+
+
+    /**
+     * //每一个棋子的权重
+     */
+    private final static int[] evaluation = {
+            0,  0,    0,    0,    0,    0,   0,   0,     0,
+            0,  100,  -8,   10,   5,    5,   10,  -8,    100,
+            0,  -8,   -45,  1,    1,    1,   1,   -45,   -8,
+            0,  10,   1,    3,    2,    2,   3,   1,     10,
+            0,  5,    1,    2,    1,    1,   2,   1,     5,
+            0,  5,    1,    2,    1,    1,   2,   1,     5,
+            0,  10,   1,    3,    2,    2,   3,   1,     10,
+            0,  -8,   -45,  1,    1,    1,   1,   -45,   -8,
+            0,  100,  -8,   10,   5,    5,   10,  -8,    100,
+            0,  0,    0,    0,    0,    0,   0,   0,     0,     0
+    };
+
+    /**
+     * 位置估值
+     * @param chess
+     * @param player
+     * @return
+     */
+    private static int posEvaluation(byte[] chess, byte player){
+        int count = 0;
+        for (byte i = 0; i < MODEL; i++) {
+            if (chess[i] == player){
+                count += evaluation[i];
+            }
+        }
+        return count;
+    }
+}
--- reversi/src/main/java/arithmetic/evaluation/ReversiEvaluation.java
+++ reversi/src/main/java/arithmetic/evaluation/ReversiEvaluation.java
@@ -22,6 +22,8 @@ public class ReversiEvaluation {
      */
     private static int count;
 
+    private static Evaltion evaltion = new CalculationEvaltion();
+
     /**
      * 终局估值 返回最终估值
      * @param data
@@ -58,72 +60,12 @@ public class ReversiEvaluation {
                 if (weight == 0){
                     continue;
                 }
-                score += weightScore(weightEnum,weight,data);
+                score += evaltion.weightScore(weightEnum,weight,data);
             }
             return score;
         }
     }
 
-    /**
-     * 根据权重类型获取分数
-     * @param weightEnum
-     * @param data
-     * @return
-     */
-    public static final int weightScore(WeightEnum weightEnum,int weight, BoardChess data){
-        byte[] chess = data.getChess();
-        byte player = data.getCurrMove(), other = player == Constant.WHITE ? Constant.BLACK : Constant.WHITE;
-        switch (weightEnum){
-            case MOBILITY:
-                return weight * (data.getOurMobility() - data.getOppMobility());
-            case POSVALUE:
-                return weight * (posEvaluation(chess,player) - posEvaluation(chess,other));
-            case COUNT:
-                return (player == Constant.WHITE ? weight : -weight) * (data.getwCount() - data.getbCount());
-            case STABISTOR:
-                return (player == Constant.WHITE ? weight : -weight) * (data.getwStators().size() - data.getbStators().size());
-            case INNER:
-                return 0;
-            case FRONTIERS:
-                return 0;
-            case PARITY:
-                return 0;
-            default:break;
-        }
-        return 0;
-    }
-
-    /**
-     * //每一个棋子的权重
-     */
-    private final static int[] evaluation = {
-            0,  0,    0,    0,    0,    0,   0,   0,     0,
-            0,  100,  -8,   10,   5,    5,   10,  -8,    100,
-            0,  -8,   -45,  1,    1,    1,   1,   -45,   -8,
-            0,  10,   1,    3,    2,    2,   3,   1,     10,
-            0,  5,    1,    2,    1,    1,   2,   1,     5,
-            0,  5,    1,    2,    1,    1,   2,   1,     5,
-            0,  10,   1,    3,    2,    2,   3,   1,     10,
-            0,  -8,   -45,  1,    1,    1,   1,   -45,   -8,
-            0,  100,  -8,   10,   5,    5,   10,  -8,    100,
-            0,  0,    0,    0,    0,    0,   0,   0,     0,     0
-    };
-
-    /**
-     * 位置估值
-     * @param chess
-     * @param player
-     * @return
-     */
-    private static int posEvaluation(byte[] chess, byte player){
-        int count = 0;
-        for (byte i = 0; i < MODEL; i++) {
-            if (chess[i] == player){
-                count += evaluation[i];
-            }
-        }
-        return count;
-    }
 
     /**
      * /棋子统计方法
